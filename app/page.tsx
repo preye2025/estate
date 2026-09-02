b54873351d3ec59cf2e3030c7661d28a31d78444
preye2025 <preyeprosper6@gmail.com>
2026-09-02 04:53:44 -0700

    third-push

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col  p-10 bg-[#FFF8DB] gap-8">
       <div className="flex flex-row items-center justify-between gap-15">
         <p className="text-[#EE7421] text-xl font-bold">EstateEaze</p>
-        <Link href="/createAccount" className="bg-[#EE7421] rounded-2xl text-white p-2">Create Account</Link>
+        <Link href="/createAccount" className="bg-[#EE7421] rounded-2xl text-white p-2 text-[14px]">Create Account</Link>
       </div>
       <div className="flex flex-col gap-3">
         <p>Everything at your fingertips</p>
@@ -19,7 +19,7 @@ export default function Home() {
       <p className="text-[14px]">EstateEaze gives you a feel of heaven</p>
       <div className="flex flex-row items-center gap-3">
         <Link href="/login" className="bg-[#fff7f2] rounded-2xl text-black p-3 pl-12 pr-12 border-[#EE7421] border-2 ">Login</Link>
-        <Link href="/createAccount" className="bg-[#EE7421] rounded-2xl text-white p-3.5">Create Account</Link>
+        <Link href="/createAccount" className="bg-[#EE7421] rounded-2xl text-white p-3.5 text-[14px]">Create Account</Link>
       </div>
     </div>
   );
